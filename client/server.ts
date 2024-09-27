197aa50564549168479c972e66ad23e573221ad4
Ayoub Alfurjani <a.alfurjani@dojoon.ly>
2024-09-27 15:58:59 +0200

    before trial

--- client/server.ts
+++ client/server.ts
@@ -3,6 +3,62 @@
 Bun.serve({
   port: 3000,
   async fetch(req) {
+    console.log(req.url);
+    if (req.url.endsWith('/')) {
+      return new Response(
+        `
+        <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body
+    style="
+      background-color: black;
+      width: 100vw;
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      box-sizing: border-box;
+      padding: 0px;
+      margin: 0px;
+      color: white;
+      font-family: Arial, Helvetica, sans-serif;
+      font-size: 5rem;
+    "
+  >
+    <button onclick="onClick()">generate</button>
+    <script src="client.js"></script>
+  </body>
+</html>
+
+`,
+        {
+          headers: {
+            'Content-Type': 'text/html',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+        }
+      );
+    }
+
+    if (req.url.endsWith('/client.js')) {
+      const file = Bun.file('./index.js');
+
+      const text = await file.text();
+      return new Response(text, {
+        headers: {
+          'Content-Type': 'application/javascript',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        },
+      });
+    }
+
     if (req.url.endsWith('/api/cat-names')) {
       const file = Bun.file('./cat-names.txt');
 
