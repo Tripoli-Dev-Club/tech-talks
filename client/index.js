b3263e350986ed9207296ec42d08d231641b5ca5
Ayoub Alfurjani <ayoub.alfurjani@gmail.com>
2024-11-08 08:47:11 +0200

    merge code

--- client/index.js
+++
@@ -1,9 +0,0 @@
-// const catNames = ['سنبوسة', 'مكرونة', 'أرز بلبن', 'كفته', 'برميل', 'طنجرة'];
-
-async function onClick() {
-  const response = await fetch('http://localhost:3000/api/cat-names');
-  const { catNames } = await response.json();
-  const index = Math.floor(Math.random() * catNames.length);
-  const catName = catNames[index];
-  document.body.innerText = catName;
-}
--- server-components/app/GenerateButton.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function GenerateButton({ catName }: { catName: string }) {
-  function onClick() {
-    document.body.innerText = catName;
-  }
-
-  return <button onClick={onClick}>generate</button>;
-}
