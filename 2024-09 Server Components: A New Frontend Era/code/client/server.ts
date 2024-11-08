6b935d1c44fe6303469dda2a819acffb2db99a6a
Ayoub Alfurjani <ayoub.alfurjani@gmail.com>
2024-11-08 08:49:32 +0200

    🔥 Code merging done more right

---
+++ 2024-09 Server Components: A New Frontend Era/code/client/server.ts
@@ -0,0 +1,81 @@
+// Create the server and define the API route
+
+Bun.serve({
+  port: 3000,
+  async fetch(req) {
+    if (req.url.endsWith('/')) {
+      const file = Bun.file('./cat-names.txt');
+
+      const text = await file.text();
+      const catNames = text.split('\n');
+      const index = Math.floor(Math.random() * catNames.length);
+      const catName = catNames[index];
+      return new Response(
+        `
+        <!DOCTYPE html>
+          <html lang="en">
+            <head>
+              <meta charset="UTF-8" />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <link rel="stylesheet" href="styles.css" />
+              <title>Document</title>
+            </head>
+            <body>
+              <button onclick="onClick()">generate</button>
+              <script>
+                function onClick() {
+                  const {catName} = ${JSON.stringify({ catName })};
+                  document.body.innerText = catName;
+                }
+              </script>
+            </body>
+          </html>
+`,
+        {
+          headers: {
+            'Content-Type': 'text/html',
+          },
+        }
+      );
+    }
+
+    if (req.url.endsWith('/client.js')) {
+      const file = Bun.file('./index.js');
+
+      const text = await file.text();
+      return new Response(text, {
+        headers: {
+          'Content-Type': 'application/javascript',
+        },
+      });
+    }
+
+    if (req.url.endsWith('/api/cat-names')) {
+      const file = Bun.file('./cat-names.txt');
+
+      const text = await file.text();
+      const catNames = text.split('\n');
+
+      return new Response(JSON.stringify({ catNames }), {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    if (req.url.endsWith('/styles.css')) {
+      const file = Bun.file('./styles.css');
+
+      const text = await file.text();
+
+      return new Response(text, {
+        headers: {
+          'Content-Type': 'text/css',
+        },
+      });
+    }
+    return new Response('Not Found', { status: 404 });
+  },
+});
+
+console.log('Server running at http://localhost:3000');
